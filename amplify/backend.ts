0e440a4d83992a193bcfdcb51a289afc4d16e87e
TodorFilipovZetta <todor.filipov@zetta.systems>
2024-12-12 17:05:26 +0200

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,17 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { Tags } from 'aws-cdk-lib';
 
-defineBackend({
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ */
+const backend = defineBackend({
   auth,
   data,
 });
+
+const tags = Tags.of(backend.stack);
+// add a new tag
+tags.add('owner', 'indigo');
+tags.add('environment', 'sandbox');
